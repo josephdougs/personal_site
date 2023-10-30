ed261084e7ca11446621c9962dd69ec572588059
Joey <josephdougs@gmail.com>
2023-10-29 17:26:09 -0700

    Create non-functional drop down

--- personal_site/app/mainApp.tsx
+++ personal_site/app/mainApp.tsx
@@ -1,5 +1,33 @@
+"use client";
+import { useEffect, useState } from 'react';
+import './general.css';
+
 export default function JoeyDrop() {
-    return (
-      <b>wassup</b>
-    );
+  const [dropVal, setDropValue] = useState<JSX.Element>();
+  const [isDropped, setIsDropped] = useState<boolean>(false);
+
+  var dropDown = (<div className="joeyDropDown">
+      <div>option 1</div>
+      <div>option 2</div>
+      <div>option C</div>
+      <div>option D</div>
+    </div>
+  );
+
+  useEffect(() => {
+    if (isDropped) {
+      setDropValue(dropDown);
+    } else {
+      setDropValue(<></>);
+    }
+  }, [isDropped]);
+
+  function handleClick() {
+    setIsDropped(!isDropped);
+    return;
   }
+
+  return (
+    <div onClick={handleClick}><span>dropMe</span>{dropVal}</div>
+  );
+}
