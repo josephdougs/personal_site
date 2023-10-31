4e1cd530563995957d07074ee9d62e95029a18a2
Joey <josephdougs@gmail.com>
2023-10-30 22:01:34 -0700

    New page structure

---
+++ personal_site/pages/home/index.js
@@ -0,0 +1,26 @@
+export default function HomePage() {
+    return (
+<div class="container">
+    <div class="borderArea">
+        <div class="sideMenu">
+            <div class="sideAnimationOverlay">
+                <div class="titleMenu">
+                    <div class="title">
+                        Joey Smith
+                    </div>
+                    <div class="menuOuter">
+                    </div>
+                </div>
+                <div class="hamburger">
+                </div>
+            </div>
+        </div>
+        <div class="bottomBorder">
+            <a class="linkedIn" href="https://www.linkedin.com/in/joeydsmith/">LinkedIn</a>
+            <a class="gitHub" href="https://github.com/josephdougs">GitHub</a>
+            <a class="resume" href="./Joseph_D_Smith_resume.pdf">Resume</a>
+        </div>
+    </div>
+</div>
+    );
+}
