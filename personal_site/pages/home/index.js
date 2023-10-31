c4fda9fb466a07ab44c1181d15189cf4f9b5e912
Joey <josephdougs@gmail.com>
2023-10-30 23:40:18 -0700

    bottom generally working

--- personal_site/pages/home/index.js
+++ personal_site/pages/home/index.js
@@ -1,3 +1,5 @@
+import '../../app/general.css';
+
 export default function HomePage() {
     return (
 <div class="container">
@@ -6,7 +8,7 @@ export default function HomePage() {
             <div class="sideAnimationOverlay">
                 <div class="titleMenu">
                     <div class="title">
-                        Joey Smith
+                        <h1>Joey Smith</h1>
                     </div>
                     <div class="menuOuter">
                     </div>
@@ -15,10 +17,28 @@ export default function HomePage() {
                 </div>
             </div>
         </div>
+        <div class="mainContent">
+            <div class="contentItem">
+                <h3>Item1</h3>
+                <p>Hello my friends how are you?</p>
+            </div>
+            <div class="contentItem">
+                <h3>Item2</h3>
+                <p>Hello my friends how are you?</p>
+            </div>
+            <div class="contentItem">
+                <h3>Item3</h3>
+                <p>Hello my friends how are you?</p>
+            </div>
+            <div class="contentItem">
+                <h3>Item4</h3>
+                <p>Hello my friends how are you?</p>
+            </div>
+        </div>
         <div class="bottomBorder">
-            <a class="linkedIn" href="https://www.linkedin.com/in/joeydsmith/">LinkedIn</a>
-            <a class="gitHub" href="https://github.com/josephdougs">GitHub</a>
-            <a class="resume" href="./Joseph_D_Smith_resume.pdf">Resume</a>
+            <a class="bottomLink resume" href="./Joseph_D_Smith_resume.pdf">Resume</a>
+            <a class="bottomLink linkedIn" href="https://www.linkedin.com/in/joeydsmith/">LinkedIn</a>
+            <a class="bottomLink gitHub" href="https://github.com/josephdougs">GitHub</a>
         </div>
     </div>
 </div>
