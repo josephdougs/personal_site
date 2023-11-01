62ab6c1acaac3146bc4d7b0c60e2e991b43e3acf
Joey <josephdougs@gmail.com>
2023-11-01 01:14:57 -0700

    Game workign as iframe

--- personal_site/pages/home/games.tsx
+++ personal_site/pages/home/games.tsx
@@ -1,6 +1,9 @@
+import BikeJS from '../../public/games/bike';
+
 export default function Games() {
     return (
         <div className="gamesContent">
             Here is games
+            <iframe src="./games/bike.html" className='gamesIFrame'></iframe>
         </div>
     )};
